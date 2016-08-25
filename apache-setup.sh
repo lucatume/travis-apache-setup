74b1be7458d3d3ecd22021bdd7ce038707a645f6
Luca Tumedei <luca@theaveragedev.com>
2016-08-25 17:53:37 +0200

    updated with more robust path finding script

--- apache-setup.sh
+++ apache-setup.sh
@@ -1,65 +1,74 @@
 #!/bin/bash
 
-siteDir="/tmp/site"
-siteUrl="http://site.dev"
-siteHost="127.0.0.1";
+SITE_DIR="/tmp/site"
+SITE_URL="http://site.dev"
+SITE_HOST="127.0.0.1";
 
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 while getopts "f:u:h:" opt; do
     case "$opt" in
-    f)  siteDir=$OPTARG
+    f)  SITE_DIR=$OPTARG
         ;;
-    u)  siteUrl=$OPTARG
+    u)  SITE_URL=$OPTARG
         ;;
-    h)  siteHost=$OPTARG
+    h)  SITE_HOST=$OPTARG
         ;;
     esac
 done
 
-breath="\n\n"
-sep="================================================================================\n"
+BREATH="\n\n"
+SEP="================================================================================\n"
 
-printf $breath
-echo "Site from $siteDir will be served on $siteUrl (resolving domain name to $siteHost address)"
-printf $sep
+printf $BREATH
+echo "Site from $SITE_DIR will be served on $SITE_URL (resolving domain name to $SITE_HOST address)"
+printf $SEP
 
-shift $((OPTIND-1))
+shsiteHost
+siteHost
+siteHost
+siteHostift $((OPTIND-1))
 
-scriptDir="$(dirname "$(readlink -f "$0")")"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-echo "Script executing from path $scriptDir"
-printf $breath
+echo "Script executing from path $SCRIPT_DIR"
+printf $BREATH
 echo "Updating and installing apache2 packages"
-printf $sep
+printf $SEP
 
 apt-get update
 apt-get install -y --force-yes apache2 libapache2-mod-fastcgi make
-apt-get install -y --force-yes php5-dev php-pear php5-mysql php5-csiteUrl php5-gd php5-json php5-sqlite php5-pgsql
+apt-get install -y --force-yes php5-dev php-pear php5-mysql php5-cSITE_URL php5-gd php5-json php5-sqlite php5-pgsql
 a2enmod headers
 
-printf $breath
+printf $BREATH
 echo "Enabling php-fpm"
-printf $sep
+printf $SEP
 
 # credit: https://www.marcus-povey.co.uk/2016/02/16/travisci-with-php-7-on-apache-php-fpm/
-if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then cp $scriptDir/assets/www.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/; fi
+if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then cp $SCRIPT_DIR/assets/www.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/; fi
 cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 a2enmod rewrite actions fastcgi alias
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
-printf $breath
+printf $BREATH
 echo "Configuring Apache virtual hosts"
-printf $sep
+printf $SEP
 
-cp -f $scriptDir/assets/travis-ci-apache /etc/apache2/sites-available/default
-sed -e "s?%DIR%?$siteDir?g" --in-place /etc/apache2/sites-available/default
-sed -e "s?%URL%?$siteUrl?g" --in-place /etc/apache2/sites-available/default
-echo "$siteHost $siteUrl" | tee --append /etc/hosts > /dev/null
+cp -f $SCRIPT_DIR/assets/travis-ci-apache /etc/apache2/sites-available/default
+sed -e "s?%DIR%?$SITE_DIR?g" --in-place /etc/apache2/sites-available/default
+sed -e "s?%URL%?$SITE_URL?g" --in-place /etc/apache2/sites-available/default
+echo "$SITE_HOST $SITE_URL" | tee --append /etc/hosts > /dev/null
 
-printf $breath
+printf $BREATH
 echo "Restarting Apache"
-printf $sep
+printf $SEP
 
 service apache2 restart
